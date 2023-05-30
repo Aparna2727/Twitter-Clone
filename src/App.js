053c0e217ff94038919182799a19134d73c3931b
Saurabh0241 <Saurabhsakhare0241@gmail.com>
2023-05-30 12:20:08 +0530

    upadate-create-account-page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 
 import './App.css';
 import Data from './Data';
+import CreateAccount from './Pages/Create account/Createaccount';
 import SignUp from './Pages/SignUp/signUp';
-import CreateAccount from './create account';
+
 
 function App() {
   return (
     <div className="App">
       
-     <h1>App page</h1>
+ 
      {/* <Data/> */}
-  {/* <CreateAccount/> */}
-     
-    <SignUp/>
+      
+    <CreateAccount/>
+    {/* <SignUp/> */}
       
     </div>
   );
--- src/Pages/SignUp/signUp.js
+++ src/Pages/SignUp/signUp.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {GrTwitter} from 'react-icons/gr'
+import {FcGoogle} from 'react-icons/fc'
+import TextField from '@mui/material/TextField';
 function SignUp() {
   return (
     <>
-    <div>
-        <div><GrTwitter size={50}/></div>
+    <div class>
+        <div><GrTwitter color='royalblue' size={50}/></div>
         <h1>Sign in to Twitter</h1>
-
+        <button><FcGoogle size={30}/>Sign up</button>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     </div>
     </>
   )
